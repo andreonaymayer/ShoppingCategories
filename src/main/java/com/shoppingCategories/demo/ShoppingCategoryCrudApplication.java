5602da9d09004e21d7353e756bee1e682b38457e
Andreo <andreocelular@gmail.com>
2021-12-20 11:35:58 -0300

    Initial Commit
    Empty project

---
+++ src/main/java/com/shoppingCategories/demo/ShoppingCategoryCrudApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingCategories.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCategoryCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCategoryCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoppingCategories/demo/ShoppingCategoryCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingCategories.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCategoryCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
