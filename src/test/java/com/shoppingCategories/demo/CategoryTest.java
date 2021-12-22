f1629eb4caf25749660ea71ca0b5e1ba1d29e542
Andreo <andreocelular@gmail.com>
2021-12-22 13:26:28 -0300

    Refactor the tests of services and repository

--- src/test/java/com/shoppingCategories/demo/CategoryTest.java
+++ src/test/java/com/shoppingCategories/demo/CategoryTest.java
@@ -1,76 +1,71 @@
-//package com.shoppingCategories.demo;
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.is;
-//import com.shoppingCategories.Repository.CategoryRepository;
-//import com.shoppingCategories.Services.CategoryService;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//public class CategoryTest {
-//
-//    @Test
-//    @DisplayName("Add 2 categories to list, and test if works")
-//    public void insertCategoryInList(){
-//        CategoryRepository categoryRepo = new CategoryRepository();
-//
-//        categoryRepo.addCategory("Category 1");
-//        categoryRepo.addCategory("Category 2");
-//
-//        int actual = categoryRepo.getCategoryList().size();
-//
-//        assertThat("The number of categories is correct", actual, is(2));
-//    }
-//
-//    @Test
-//    @DisplayName("Verify if already have other category with same name")
-//    public void verifyIfExistsCategoryName(){
-//        CategoryRepository categoryRepo = new CategoryRepository();
-//        categoryRepo.addCategory("Category 1");
-//        categoryRepo.addCategory("Category 2");
-//
-//        boolean actual = categoryRepo.nameAlreadyExists("Category 1");
-//
-//        assertThat("nameAlreadyExists return the index item, or -1 if this item don't exists", actual, is(true));
-//    }
-//
-//    @Test
-//    @DisplayName("List all categories of repository")
-//    public void testCategoryList(){
-//        CategoryRepository categoryRepo = new CategoryRepository();
-//
-//        //categoryRepo.addCategory("Category 1");
-//        //categoryRepo.addCategory("Category 2");
-//
-//        CategoryService categoryService = new CategoryService(categoryRepo);
-//
-//        System.out.println(categoryService.listToString());
-//    }
-//
-//    @Test
-//    @DisplayName("Delete Category by name")
-//    public void testCategoryDelete(){
-//        CategoryRepository categoryRepo = new CategoryRepository();
-//
-//        categoryRepo.deleteCategory("Food");
-//
-//        CategoryService categoryService = new CategoryService(categoryRepo);
-//
-//        System.out.println("RESULT:\n" + categoryService.listToString());
-//        int actual = categoryRepo.getCategoryList().size();
-//        assertThat("The number of categories is correct", actual, is(2));
-//    }
-//
-//
-//    @Test
-//    @DisplayName("Edit Category")
-//    public void editCategory() {
-//        CategoryRepository categoryRepo = new CategoryRepository();
-//
-//        //categoryRepo.editCategory("Food","Sports");
-//        String expected = categoryRepo.getCategoryList().get(0).getName();
-//
-//        assertThat("The number of categories is correct", expected, is("Sports"));
-//
-//    }
-//
-//}
+package com.shoppingCategories.demo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import com.shoppingCategories.Entities.Category;
+import com.shoppingCategories.Repository.CategoryRepository;
+import com.shoppingCategories.Services.CategoryService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+
+    @Test
+    @DisplayName("Add 2 categories to list, and test if works")
+    public void insertCategoryInList() {
+        CategoryRepository categoryRepo = new CategoryRepository();
+        categoryRepo.addCategory(new Category("", "Category 1"));
+        categoryRepo.addCategory(new Category("", "Category 2"));
+        int actual = categoryRepo.getCategoryList().size();
+
+        assertThat("The number of categories is correct", actual, is(7));
+    }
+
+    @Test
+    @DisplayName("Verify if already have other category with same name")
+    public void verifyIfExistsCategoryName() {
+        CategoryRepository categoryRepo = new CategoryRepository();
+        categoryRepo.addCategory(new Category("", "Category 1"));
+
+        boolean actual = categoryRepo.nameAlreadyExists("Category 1");
+
+        assertThat("nameAlreadyExists return the index item, or -1 if this item don't exists", actual, is(true));
+    }
+
+    @Test
+    @DisplayName("List all categories of repository")
+    public void testCategoryList(){
+        CategoryRepository categoryRepo = new CategoryRepository();
+        CategoryService categoryService = new CategoryService(categoryRepo);
+
+        System.out.println(categoryService.listToString());
+    }
+
+    @Test
+    @DisplayName("Delete Category by name")
+    public void testCategoryDelete(){
+        CategoryRepository categoryRepo = new CategoryRepository();
+
+        categoryRepo.deleteCategory("Food");
+
+        CategoryService categoryService = new CategoryService(categoryRepo);
+
+        System.out.println("RESULT:\n" + categoryService.listToString());
+        int actual = categoryRepo.getCategoryList().size();
+        assertThat("The number of categories is correct", actual, is(4));
+    }
+
+    @Test
+    @DisplayName("Edit Category")
+    public void editCategory() {
+        CategoryRepository categoryRepo = new CategoryRepository();
+        Category computers = new Category("", "Personal computers");
+        categoryRepo.editCategory(computers,"Food");
+
+        String expected = categoryRepo.getCategoryList().get(0).getName();
+
+        assertThat("The name is not the same", expected, is("Personal computers"));
+
+    }
+
+}
