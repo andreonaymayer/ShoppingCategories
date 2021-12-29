ce1e106b94dcea138eac844003c43da5699c1f98
Andreo <andreocelular@gmail.com>
2021-12-29 18:22:39 -0300

    Insert JWT Working

---
+++ src/main/java/com/shoppingCategories/Entities/AuthenticationResponse.java
@@ -0,0 +1,17 @@
+package com.shoppingCategories.Entities;
+
+import java.io.Serializable;
+
+public class AuthenticationResponse implements Serializable {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
+
